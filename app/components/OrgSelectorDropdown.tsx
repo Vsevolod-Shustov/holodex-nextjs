bd75aec0710a8c421ca630fc3a4c1fdd45b76910
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-12-08 09:56:48 +0300

    extracted org selector dropdown to a separate component

---
+++ app/components/OrgSelectorDropdown.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from "react";
+import Dropdown from "@/app/ui/Dropdown";
+
+const items = [
+  { title: "All Vtubers", value: "All" },
+  { title: "Independents", value: "Independents" },
+  { title: "Hololive", value: "Hololive" },
+  { title: "Nijisanji", value: "Nijisanji" },
+]
+
+const queryOrg = "Hololive"
+
+export default function OrgSelectorDropdown() {
+  const [org, setOrg] = useState(queryOrg)
+  return (
+    <Dropdown title={org} closeOnInsideClick={true}>
+      <div className="rounded bg-slate-900 py-1 w-max">
+        <ul>
+          {items.map((item) => (
+            <li className="hover:bg-slate-500 py-1 px-4 cursor-pointer"
+              onClick={() => { console.log(item.value); setOrg(item.value) }}
+              key={item.value}
+            >
+              {item.title}
+            </li>))}
+        </ul>
+      </div>
+    </Dropdown>
+  )
+}
