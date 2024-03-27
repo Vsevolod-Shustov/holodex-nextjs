65638f81946def4c8571aba24ffd8bc4d2577622
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2024-03-27 14:42:15 +0300

    github pages actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_PATH =
+  process.env.NODE_ENV === "production"
+    ? "/holodex-nextjs"
+    : "";
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  output: "export",
+  basePath: BASE_PATH
 }
 
 module.exports = nextConfig
